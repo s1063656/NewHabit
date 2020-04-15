ba35bdaa9d01d1990ce6a3ad25b347de29dc90e6
s1063656 <s1063656@gm.pu.edu.tw>
2020-04-15 12:03:51 +0800

    A15_02

--- app/src/main/java/com/tse/newhabit/MainActivity.java
+++ app/src/main/java/com/tse/newhabit/MainActivity.java
@@ -4,20 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
-    private HomeFragment mHomeFragment;
+public class MainActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    public static int lastPosition = 0;
+    /*private HomeFragment mHomeFragment;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mHomeFragment = new HomeFragment();
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                lastPosition = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        setupViewPager(mViewPager);
+        mTabLayout.setupWithViewPager(mViewPager);
+        /*mHomeFragment = new HomeFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.frameLayout,mHomeFragment).commitAllowingStateLoss();
+        transaction.add(R.id.frameLayout,mHomeFragment).commitAllowingStateLoss();*/
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new CreateFragment(), "Create");
+        adapter.addFragment(new InfoFragment(), "Info");
+
+        viewPager.setAdapter(adapter);
     }
 }
